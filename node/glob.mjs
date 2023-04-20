51650b7d4577da2685de8095deaa9fbaa026c26a
isaacs <i@izs.me>
2023-04-20 11:04:41 -0700

    add node

---
+++ node/glob.mjs
@@ -0,0 +1,15 @@
+import { minimatch } from 'minimatch'
+import { relative } from 'node:path'
+import {fileURLToPath} from 'node:url'
+
+const [_, __, glob, filename] = process.argv
+if (glob === undefined || filename === undefined) {
+  console.error(`Usage: ${
+    relative(process.cwd, fileURLToPath(import.meta.url))
+  } <glob> <filename>`)
+  process.exit(1)
+}
+
+console.log(`${JSON.stringify(filename)} ${
+  minimatch(filename, glob) ? 'matches' : 'does not match'
+} ${JSON.stringify(glob)}`)
